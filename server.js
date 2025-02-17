817ca570eab9f5bce5124fca931664af65cd6e00
Cesar Preza <cpreza93@yahoo.com>
2025-02-16 20:51:24 -0800

    adds route to search for a fruit

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const axios = require('axios');
 require('dotenv').config;
 
 const app = express();
-const BASE_URL = 'https://www.fruityvice.com/api/fruit/all';
+const BASE_URL = 'https://www.fruityvice.com/api/fruit';
 
 //Middleware
 app.use(cors());
@@ -15,11 +15,28 @@ app.get('/', (req, res) => {
 
 app.get('/fruits', async (req, res) => {
   try {
-    const response = await axios.get(BASE_URL);
+    const response = await axios.get(`${BASE_URL}/all`);
     res.json(response.data);
   } catch (error) {
     console.error(error);
     res.status(500).json({ error: 'Failed to fetch data' });
+  }
+});
+
+//Search Route
+app.get('/fruits', async (req, res) => {
+  try {
+    const { name } = req.query;
+
+    if (!name) {
+      return res.status(400).json({ error: 'name is required' });
+    }
+
+    const response = await axios.get(`${BASE_URL}/${name.toLowerCase()}`);
+    const data = response.data;
+    res.json(response.data);
+  } catch (error) {
+    res.status(500).json({ error: 'failed to fetch fruit data' });
   }
 });
 
