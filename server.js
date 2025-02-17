a7a10a0c33f20e9962047e4fbbb13c36834c57ae
Cesar Preza <cpreza93@yahoo.com>
2025-02-16 19:20:34 -0800

    adds data from fruits api

--- server.js
+++ server.js
@@ -3,15 +3,24 @@ const cors = require('cors');
 const axios = require('axios');
 require('dotenv').config;
 
-const port = process.env.PORT;
-
 const app = express();
+const BASE_URL = 'https://www.fruityvice.com/api/fruit/all';
 
 //Middleware
 app.use(cors());
 
 app.get('/', (req, res) => {
   res.send({ message: 'welcome to the backend' });
+});
+
+app.get('/fruits', async (req, res) => {
+  try {
+    const response = await axios.get(BASE_URL);
+    res.json(response.data);
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ error: 'Failed to fetch data' });
+  }
 });
 
 app.listen(process.env.PORT || 3000, () => {
